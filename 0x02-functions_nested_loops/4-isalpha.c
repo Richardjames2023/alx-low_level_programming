f320c563d29658fe3263f5fed62f4cdda9f55a2b
Richardjames2023 <richardjames2023@gmail.com>
2023-07-30 01:57:23 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
-*_isalpha - check for alphabetic character 
-*@c: The character to be checked
-*Return: 1 for alphabetic character or 0 for anything else
-*/
+ * _isalpha - check for alphabetic character
+ * @c: the character to be checked
+ * Return: 1 for alphabetic character or 0 for anything else
+ */
 
 int _isalpha(int c)
 {
